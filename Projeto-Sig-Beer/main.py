a555298ac201439092313afeb440bbea819dc586
ThomasReyel <thomasreyel2231@gmail.com>
2025-06-24 15:36:15 -0300

    Commit Inicial

---
+++ Projeto-Sig-Beer/main.py
@@ -0,0 +1,8 @@
+# Assinante (Id, Nome, contato (email), endereço)
+# Assinatura (ID, Id(Assinante), id(plano))
+# Planos (id, Id(Produto), preço, tempo)
+# produto (id, nome do produto, tipo)
+
+#Funcionamento:
+# Módulo de assinante (Cadastro, assinar, checar assinatura, alterar assinatura, cancelar assinatura)
+# Módulo do Admin (Criar planos / produtos, alterar Planos/produtos, checar assinaturas, cancelar tudo)
