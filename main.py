78c3180906b8f512170cc9a230ffa9fe5d24e1a7
ThomasReyel <thomasreyel2231@gmail.com>
2025-06-25 13:34:03 -0300

    Alteracao do layout do promt

--- main.py
+++ main.py
@@ -5,41 +5,86 @@
 
 #Funcionamento:
 # Módulo de assinante (Cadastro, assinar, checar assinatura, alterar assinatura, cancelar assinatura)
-# Módulo do Admin (Criar planos / produtos, alterar Planos/produtos, checar assinaturas, cancelar tudo)
+# Módulo do Admin (Criar planos, alterar Planos, checar assinaturas, cancelar tudo)
 # Projeto Sig-Beer: Um Sistema de Assinatura de Cervejas
 cntrl_resp_global = 0
 cntrl_resp_cli = 0
+cntrl_resp_adm = 0
+
 while cntrl_resp_global == 0:
-    print("_______________________________________________________________")
-    aux = int(input('Olá, seja bem-vindo ao Sig-Beer!\nQual módulo deseja entrar?\n1 - Cliente\n2 - Admin\n3 - Sair\nDigite aqui: '))
+    print("________________________________________________")
+    print("|                                              |")
+    print("| Olá, seja bem-vindo ao Sig-Beer!             |")
+    print("| Qual módulo deseja entrar?                   |")
+    print("|                                              |")
+    print("| 1 - Cliente                                  |")
+    print("| 2 - Admin                                    |")
+    print("| 3 - Sair                                     |")
+    print("|                                              |")
+    aux = int(input('| Digite aqui: '))
+    print("|______________________________________________|")
+
     match aux:
         case 1:
             while cntrl_resp_cli == 0:
-                print('\n\n\n\n\n\n\n\n\n\n')
+                print('\n\n\n\n\n\n\n\n\n\n') 
                 print("_______________________________________________________________")
-                print('Olá, você entrou no módulo de Cliente, o que você deseja fazer')
-                aux = int(input('1 - Cadastrar\n2 - Checar conta\n3 - Alterar assinatura\n4 - Cancelar assinatura\n5 - Voltar'))
+                print("|                                                             |")
+                print("| Olá, você entrou no módulo de Cliente!                      |")
+                print("| O que você deseja fazer?                                    |")
+                print("| 1 - Cadastrar                                               |")
+                print("| 2 - Checar conta                                            |")
+                print("| 3 - Alterar assinatura                                      |")
+                print("| 4 - Cancelar assinatura                                     |")
+                print("| 5 - Voltar                                                  |")
+                print("|                                                             |")
+                aux = int(input('| Digite aqui: '))
+                print("|_____________________________________________________________|")
                 match aux:
                     case 1:
-                        ...
+                        print("\n_____ Cadastrar Cliente _____\n")
                     case 2:
-                        ...
+                        print("\n_____ Checar Conta Cliente _____\n")
                     case 3:
-                        ...
+                        print("\n_____ Alterar Assinatura Cliente _____\n")
                     case 4:
-                        ...
+                        print("\n_____ Cancelar Assinatura Cliente _____\n")
                     case 5:
-                        ...
+                        cntrl_resp_cli = 1 
                     case _:
-                        print('Você não digitou uma opção válida, tente novamente')
-
-                
+                        print('\nVocê não digitou uma opção válida, tente novamente')
+            cntrl_resp_cli = 0 
         case 2:
-            print('\n\n\n\n\n\n\n\n\n\n')
-            print("_______________________________________________________________")
-            print('Olá, você entrou no módulo de Administrador, o que você deseja fazer')
+            while cntrl_resp_adm == 0:
+                print('\n\n\n\n\n\n\n\n\n\n')
+                print("_______________________________________________________________")
+                print("|                                                             |")
+                print("| Olá, você entrou no módulo de Administrador                 |")
+                print("| O que você deseja fazer?                                    |")
+                print("| 1 - Cadastrar plano                                         |")
+                print("| 2 - Checar planos                                           |")
+                print("| 3 - Alterar planos                                          |")
+                print("| 4 - Excluir planos                                          |")
+                print("| 5 - Voltar                                                  |")
+                print("|                                                             |")
+                aux = int(input('| Digite aqui: '))
+                print("|_____________________________________________________________|")
+                match aux:
+                    case 1:
+                        print("\n_____ Cadastrar Plano _____\n")
+                    case 2:
+                        print("\n_____ Checar Planos _____\n")
+                    case 3:
+                        print("\n_____ Alterar Planos _____\n")
+                    case 4:
+                        print("\n_____ Excluir Planos _____\n")
+                    case 5:
+                        cntrl_resp_adm = 1 
+                    case _:
+                        print('\nVocê não digitou uma opção válida, tente novamente')
+            cntrl_resp_adm = 0 
         case 3:
-            print('Volte sempre')
-            cntrl_resp_global=1        
+            print('\nVolte sempre!')
+            cntrl_resp_global = 1
         case _:
-            print('Você não digitou uma opção válida, tente novamente')
+            print('\nVocê não digitou uma opção válida, tente novamente')
